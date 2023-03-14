9f0dbdaec6de7d23b7fb599bfd0ba2b2dfb59723
Avishka Umesh <umesh.avishka@gmail.com>
2023-03-14 09:40:18 +0530

    added auto login

--- app/src/main/java/com/example/logintest/MainActivity.kt
+++ app/src/main/java/com/example/logintest/MainActivity.kt
@@ -5,8 +5,12 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -14,12 +18,20 @@ class MainActivity : AppCompatActivity() {
         //this use to hide action bar
         supportActionBar?.hide()
 
+        auth = Firebase.auth
+
         //use to put delay
         Handler(Looper.getMainLooper()).postDelayed({
-            //use to start main activity
-            val intent = Intent(this, SignInActivity::class.java)
-            startActivity(intent)
-
+            val user = auth.currentUser
+            if (user != null) {
+                //use to start home activity
+                val intent = Intent(this, HomeActivity::class.java)
+                startActivity(intent)
+            } else {
+                //use to start main activity
+                val intent = Intent(this, SignInActivity::class.java)
+                startActivity(intent)
+            }
             //use to destroy current activity
             finish()
         }, 2000)
